ac2959082eec777ae969a94d8347e96414794297
Octavian Maier <Octavian.Maier@ibm.com>
2022-05-30 13:10:47 +0300

    remove terraform unused folder

--- cli/terraform/modules/ansible-exec/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "IP" {
-    type = string
-    description = "IP used to execute ansible"
-}
-
-variable "sap_master_password" {
-    type = string
-    description = "hana_master_password"
-}
